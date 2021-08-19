2a9af945b6896429e8475da49bc344075b797f3a
sevader14 <vsevolod.deriushkin@gmail.com>
2021-08-19 16:28:31 +0200

    adds styling to table

--- src/views/ArticlesDashboard.jsx
+++ src/views/ArticlesDashboard.jsx
@@ -19,9 +19,38 @@ const useStyles = makeStyles({
     marginLeft: '200px',
     maxWidth: '1720px',
   },
+  tableHeader: {
+    backgroundColor: '#FBBA00',
+    color: 'white',
+  },
   column1: {
-    maxWidth: '2rem',
-  }
+    width: '1rem',
+  },
+  column2: {
+    width: 'auto',
+    fontWeight: 800,
+  },
+  column3: {
+    maxWidth: '1rem',
+  },
+  column1Header: {
+    width: '1rem',
+    color: 'white',
+    fontWeight: 800,
+    fontSize: '1.2rem',
+  },
+  column2Header: {
+    width: 'auto',
+    color: 'white',
+    fontWeight: 800,
+    fontSize: '1.2rem',
+  },
+  column3Header: {
+    maxWidth: '1rem',
+    color: 'white',
+    fontWeight: 800,
+    fontSize: '1.2rem',
+  },
 })
 
 const ArticlesDashboard = () => {
@@ -32,6 +61,16 @@ const ArticlesDashboard = () => {
     Articles.index()
   }, [])
 
+  const tableHeader = (
+    <TableRow>
+      <TableCell className={classes.column1Header}>Status</TableCell>
+      <TableCell className={classes.column2Header}>Title</TableCell>
+      <TableCell className={classes.column3Header}>Author</TableCell>
+      <TableCell className={classes.column3Header}>Date</TableCell>
+      <TableCell className={classes.column3Header}>Action</TableCell>
+    </TableRow>
+  )
+
   const tableRows = articles.map((article) => {
     const { id, title, author, date, publish } = article
     return (
@@ -40,13 +79,13 @@ const ArticlesDashboard = () => {
           <FormControlLabel
             control={<Switch checked={publish} name={`publish-${id}`} />}
             label={publish ? 'Published' : 'Hidden'}
-            labelPlacement='bottom'        
+            labelPlacement='bottom'
           />
         </TableCell>
-        <TableCell>{title}</TableCell>
-        <TableCell>{author}</TableCell>
-        <TableCell>{date}</TableCell>
-        <TableCell>Placeholder</TableCell>
+        <TableCell className={classes.column2}>{title}</TableCell>
+        <TableCell className={classes.column3}>{author}</TableCell>
+        <TableCell className={classes.column3}>{date}</TableCell>
+        <TableCell className={classes.column3}>Placeholder</TableCell>
       </TableRow>
     )
   })
@@ -55,15 +94,7 @@ const ArticlesDashboard = () => {
     <>
       <TableContainer component={Paper} className={classes.tableContainer}>
         <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell className={classes.column1}>Status</TableCell>
-              <TableCell>Title</TableCell>
-              <TableCell>Author</TableCell>
-              <TableCell>Date</TableCell>
-              <TableCell>Action</TableCell>
-            </TableRow>
-          </TableHead>
+          <TableHead className={classes.tableHeader}>{tableHeader}</TableHead>
           <TableBody>{tableRows}</TableBody>
         </Table>
       </TableContainer>
