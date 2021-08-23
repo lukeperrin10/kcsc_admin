93c6b1b3810bf7d29b9b10980bf95a259ceee68f
sevader14 <vsevolod.deriushkin@gmail.com>
2021-08-23 16:36:35 +0200

    adds accordion for footer form

--- src/views/GeneralDashboard.jsx
+++ src/views/GeneralDashboard.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import { Box } from '@material-ui/core'
+import React, { useState } from 'react'
+import {
+  Box,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import useCommonStyles from '../theme/useCommonStyles'
+import app_data from '../data/add_data.json'
+import { TramRounded } from '@material-ui/icons'
 
 const GeneralDashboard = () => {
   const commonClasses = useCommonStyles()
+  const [appData, setAppData] = useState(app_data)
+  const [expanded, setExpanded] = useState({ footer: true })
+
   return (
     <>
       <Box className={commonClasses.viewContainer}>
-        <div>General View</div>
+        <Accordion
+          expanded={expanded.footer}
+          onChange={() =>
+            setExpanded({ ...expanded, footer: !expanded.footer })
+          }>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant='h5'>Footer</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
       </Box>
     </>
   )
