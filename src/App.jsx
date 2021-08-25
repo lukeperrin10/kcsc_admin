614cc16a267a9a060a574899e7fd4625338d645e
kim_haaga <kim.haaga@live.se>
2021-08-25 12:48:44 +0200

    re arrange imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import './styles/globals.css'
-import Sidebar from './components/navigation/Sidebar'
-// import AdminDashboard from './views/AdminDashboard'
-import GeneralDashboard from './views/GeneralDashboard'
-import useMediaQuery from '@material-ui/core/useMediaQuery'
-import PhoneSidebar from './components/navigation/PhoneSidebar'
-import LoginPage from './components/LoginPage'
-//import Authentication from './modules/Authentication'
 import { Route, Switch } from 'react-router-dom'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+
+import store from './state/store/configureStore'
+// import Authentication from './modules/Authentication'
+import GeneralDashboard from './views/GeneralDashboard'
 import AnalyticsDashboard from './views/AnalyticsDashboard'
 import ArticlesDashboard from './views/ArticlesDashboard'
-import store from './state/store/configureStore'
 import ArticleCreation from './views/ArticleCreation'
+import LoginPage from './components/LoginPage'
+import Sidebar from './components/navigation/Sidebar'
+import PhoneSidebar from './components/navigation/PhoneSidebar'
 import SuccessSnackbar from './components/popups/SuccessSnackbar'
 import ErrorSnackbar from './components/popups/ErrorSnackbar'
+import './styles/globals.css'
 
 const App = () => {
   const { authenticated } = useSelector((state) => state)
   const isSmall = useMediaQuery('(max-width:1280px)')
 
   useEffect(() => {
-    //Authentication.validateToken()
+    // Authentication.validateToken()
     // Following will automatically authenticate on localhost while API is not up
     store.dispatch({
       type: 'AUTHENTICATE',
