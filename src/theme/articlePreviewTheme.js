a5333e79450d5b2f4bb56e0855742a44b0ac78e7
kim_haaga <kim.haaga@live.se>
2021-08-25 16:21:23 +0200

    Update image style

--- src/theme/articlePreviewTheme.js
+++ src/theme/articlePreviewTheme.js
@@ -1,9 +1,15 @@
 import { makeStyles } from '@material-ui/core'
 
 const articlePreview = makeStyles((theme) => ({
+  modal: {
+    overflow: "scroll"
+  },
   articleContainer: {
     backgroundColor: '#fff',
     marginTop: "180px",
+    marginBottom: "180px",
+    maxWidth: "960px",
+    padding: "80px",
   },
   information: {
     marginTop: "12px",
